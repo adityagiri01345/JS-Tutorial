91a185c463876ccff832c167240d0260a20bc565
adityagiri01345 <adityagiri2809@gmail.com>
2025-11-22 16:53:18 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version
+
+//alert(3+3) // we are using nodejs, not brwser
+
+console.log(4+
+    4) // code readability should be high
+
+
+    let name ="aman"
+    let age = 56;
+    let isLoggedIn = false;
+    let state = null;
+    let cityadd;
+
+    //             Datatypes
+    // String ""
+    //bigint
+    // number => 2 to power 53
+    // boolean => True/false
+    // null => standardlone value
+    // undefined =>
+    // symbol => unique
+
+
+    // Object
+              console.log(typeof cityadd)
