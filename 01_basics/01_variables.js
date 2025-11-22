c7a57a7ef5776bedd4a6ec639c8133eed67c216d
adityagiri01345 <adityagiri2809@gmail.com>
2025-11-22 16:27:28 +0000

    container config

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accoutnId = 45623
+let accoutEmail = "Ajay@gmail.com"
+var accountPassword = "654563"
+accountCity="jaipur";
+
+//accountId =2 // we can't change the value of const
+
+
+accountEmail = "asa@gmail.com"
+accountPassword = "67386423"
+accountCity = "Bangluru";
+let accountState;
+
+/*
+we are not use var in var problem is scope  {}
+prefer not to use varbecause of issue in block scope and 
+functional scope
+
+
+*/
+console.log(accoutnId);
+console.table([accoutnId, accountPassword, accoutEmail, accountCity, accountState])
