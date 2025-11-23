3ac0d3695b119c9ac2bb6b3300dd02f22de061de
adityagiri01345 <adityagiri2809@gmail.com>
2025-11-23 02:42:42 +0000

    conversion in dataType

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -26,5 +26,45 @@ console.log(booleanIsLoggedIn);
 let sumNumber = 44;
 
 let strinNumber = String(sumNumber);
-console.log(strinNumber);
-console.log(typeof (strinNumber))
+// console.log(strinNumber);
+// console.log(typeof (strinNumber))
+
+// ---------------- OPERATION ----------------
+  let value = 3
+  let nagValue = -value
+ // console.log(nagValue)
+
+
+ //console.log(3+3)
+ //console.log(3*3)
+ //console.log(3-3)
+ //console.log(3**3)
+ //console.log(3/3)
+ //console.log(3%3)
+
+  let str1 = "hello"
+  let str2 = "Aditya"
+   let str3 = str1+str2
+   console.log(str3)
+
+   console.log("5"+ 8);
+   console.log(8 +"6");
+   console.log(2 + 8 + "6");
+console.log("5"+ 8 + 4);
+ 
+   console.log(2 + 5 * 5 % 4);
+
+   console.log(+true);
+   console.log(+"");
+
+
+   let num1, num2, num3, num4;
+
+ num1 = num2 = num3 = num4 = 4+7;
+
+ let gameCounter =100;
+ gameCounter++;
+ console.log(gameCounter);
+ 
+
+
