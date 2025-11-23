655b9392a12d97fe9f218cce7152543572d25995
adityagiri01345 <adityagiri2809@gmail.com>
2025-11-23 03:06:41 +0000

    comparision in dataTypes

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,36 @@
+// console.log(2>3);
+// console.log(2<3);
+// console.log(2>=3);
+// console.log(2==3);
+// console.log(2!=3);
+ 
+
+// console.log("5" >  4);
+// console.log("05" > 3);
+
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+
+
+// ===== // check strictly your dataType
+
+console.log("2" === 2);
+console.log(2 === 2);
+console.log(2 === "2");
+console.log("5"=== 6);
+
+
+
+
+
+
+
+
