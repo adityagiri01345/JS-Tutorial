11e93a38bbacebdd3b00d49fb153203ca6a07bfc
adityagiri01345 <adityagiri2809@gmail.com>
2025-11-23 02:15:06 +0000

    conversion in dataType

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let num = null;
+let score = undefined
+
+
+//console.log(typeof(score))
+
+//let valueInNumber= Number(score)
+//console.log( typeof num)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+
+// "55" => 55
+// "55abs" => NaN
+// true =>1 ; false => 0;
+
+let issLoggedIn = 0
+ 
+let booleanIsLoggedIn = Boolean(issLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1=> true; 0 => false;
+// "" => false
+// "hitesh" => true
+
+let sumNumber = 44;
+
+let strinNumber = String(sumNumber);
+console.log(strinNumber);
+console.log(typeof (strinNumber))
