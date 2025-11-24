0a45e33762c6e31ea3960d6ac125a34cb16e04d8
adityagiri01345 <adityagiri2809@gmail.com>
2025-11-24 03:43:22 +0000

    datatypes-conversion

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,37 @@
+// permitive 
+
+//datatypes:7 types: String, Number, Boolean, BigInt, null, undefined, Symbol
+
+ const score  =45
+   const scoreValue = 45.2
+
+   const issLoggedIn = false
+   const outsideTemp = null
+   let userEmail;
+
+   const id = Symbol('123')
+   const anotherId = Symbol('123')
+
+   console.log(id===anotherId);
+
+   const bigNumber = 5131216787745478n
+
+
+
+
+   // Refrence Type (Non primitive)
+   // Array, Objects, Functions
+
+   const heros = ["Anna", "nagraj", "doga"] // this type of data is primituve
+
+
+   let myObj = {
+    name:"Muttu",
+    age:22,
+   }
+
+   const myFunction = function(){
+    console.log("Hello world")
+   }
+
+   console.log(typeof bigNumber)
