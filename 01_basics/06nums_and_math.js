e7178bd2cd2dbf28c892d2fb1a957ea93c5ae5cc
adityagiri01345 <adityagiri2809@gmail.com>
2025-11-24 10:58:04 +0000

    06nums and math

---
+++ 01_basics/06nums_and_math.js
@@ -0,0 +1,54 @@
+const  score = 400
+
+// const balance = new Number(200)
+// console.log(balance);
+
+//  const newProperty = new Number(100)
+//  console.log(newProperty);
+
+
+//  console.log(balance.toString().length);
+     
+//  console.log(newProperty.toFixed(3))
+//  console.log(newProperty.toFixed(1))
+
+//  const otherNumber = 5525.54546
+//  console.log(otherNumber.toPrecision(3));
+
+//  const hundreds=100000000
+//  console.log(hundreds.toLocaleString())
+
+//  console.log(hundreds.toLocaleString('en-In'));
+
+
+//++++++++++++++++++++++++++
+ 
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.9));
+// console.log(Math.floor(5.9));
+// console.log(Math.min(8, 7, 6, 2, 8));
+// console.log(Math.max(9, 7, 3, 5,));
+
+ console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 70                                                                             
+const max = 90
+
+console.log(Math.floor(Math.random()*(max - min + 1 ))+min);
+
+
+
+
+
+
+
+
+
+
+ 
+
+ 
