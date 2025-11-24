6200cbb1c7f30fb8f39bf2e2650f3a2c629c5fd9
adityagiri01345 <adityagiri2809@gmail.com>
2025-11-24 08:57:44 +0000

    String

---
+++ 01_basics/05_String.js
@@ -0,0 +1,33 @@
+const name = "Akash"
+const repoCount = 45
+
+//console.log(name + repoCount + "Ajay");
+
+console.log(`Hello my my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Hitesh-code')
+
+// console.log(gameName[3]);
+// console.log(gameName.length);
+
+// console.log(gameName.toUpperCase());
+
+//  const newString = gameName.subString(0, 4)
+ //console.log(newString);
+
+ const anotherString = gameName.slice(-8, 4)
+ console.log(anotherString);
+
+
+ const myName = "     mr.Gukesh@gmail.com      "
+ console.log(myName);
+ console.log(myName.trim());
+ 
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+console.log(url.replace('%20', '-'))
+
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
