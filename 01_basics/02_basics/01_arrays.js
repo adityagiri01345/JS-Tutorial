aebaafc8ad48b5faaf5e2f34e5235b6f725da51b
adityagiri01345 <adityagiri2809@gmail.com>
2025-11-25 10:39:47 +0000

    02_basics/01_arrays.js

---
+++ 01_basics/02_basics/01_arrays.js
@@ -0,0 +1,38 @@
+// arrays
+
+const myArr = [0, 1, 2, 3, 4, 5,]
+const myFamily = ["Aditya", "Gurudeva"]
+
+const myNew = Array(1, 2, 3, 4, 5)
+//console.log(myFamily[1]);
+
+// Array methods 
+
+// myNew.push(6)
+// myNew.push(7)
+// myNew.pop()
+
+// myNew.unshift(1)
+// myNew.shift()
+
+// console.log(myNew.includes(6));
+// console.log(myNew.indexOf(5));
+
+// console.log(myNew);
+
+// const newArr = myArr.join()
+// console.log(myArr);
+// console.log(newArr);
+
+// Slice, splice  
+
+ console.log("A", myArr);
+ const myn1 = myArr.slice(1, 3)
+
+ console.log(myn1);
+ console.log("B", myArr);
+
+ const myn2 = myArr.splice(1, 3)
+ console.log(myn2);
+ 
+  console.log("C", myArr);
